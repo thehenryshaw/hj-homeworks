9073d311aeef1906f75408020842859b2ec90f0b
Igor Rodionov <henryshaw217@gmail.com>
2019-07-12 14:59:57 +0300

    Установлено соединение

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+connection.addEventListener('message', event => {
+  console.log(event.data);
+})
+
+editor.addEventListener('update', event => {
+  const canvas = event.canvas;
+  canvas.toBlob(blob => {
+    connection.send(blob);
+  })
+})
