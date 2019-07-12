9930bd888fc7e3fe59a3ae0b31cd2c814edfbc85
Igor Rodionov <henryshaw217@gmail.com>
2019-07-12 15:17:46 +0300

    Реализовал интернет-магазин

--- dom/shop/js/price-format.js
+++ dom/shop/js/price-format.js
@@ -1,3 +1,20 @@
 function getPriceFormatted(value) {
   return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
+
+const buttons = Array.from(document.getElementsByTagName('button'));
+const counter = document.getElementById('cart-count');
+const summ = document.getElementById('cart-total-price');
+
+let i = 0;
+let p = 0;
+
+function addToCart(event) {
+  counter.innerHTML = ++i;
+  let price = event.currentTarget.dataset.price;
+  p = +price + p; 
+  summ.innerHTML = getPriceFormatted(p);
+}
+
+buttons
+.forEach(el => el.addEventListener('click', addToCart));
