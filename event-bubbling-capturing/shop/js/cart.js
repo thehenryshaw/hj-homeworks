f75316836d0d6dc4a7f46c76bf7b5c2deb2457cf
Igor Rodionov <henryshaw217@gmail.com>
2019-07-15 20:33:53 +0300

    Релазивана работа корзины

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const itemsList = document.querySelector('.items-list');
+const item = document.querySelector('.badge');
+
+itemsList.addEventListener('click', event => {
+  if (event.target.classList.contains('add-to-cart')) {
+    item.textContent = ++item.textContent;
+  }
+})
