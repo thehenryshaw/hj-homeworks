f89937d7e92483bfc71bc2d066b867f5b7d3e406
Igor Rodionov <henryshaw217@gmail.com>
2019-07-23 16:23:46 +0300

    Реализовал вывод меню и секретного кода

---
+++ event-object/easter-egg/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const navigation = document.getElementsByTagName('nav')[0];
+const secret = document.getElementsByClassName('secret')[0];
+
+function openMenu(event) {
+  if (!(event.ctrlKey && event.altKey)) {
+    return;
+  }
+  if(event.code === 'KeyT') {
+    navigation.classList.toggle('visible');
+  }
+}
+
+document.addEventListener('keydown', openMenu);
+
+let secretCode = [];
+const password = ['KeyY', 'KeyT', 'KeyN', 'KeyJ', 'KeyK', 'KeyJ', 'KeyU', 'KeyB', 'KeyZ'];
+
+function showSecretCode(event) {
+  if(event.code === 'KeyY') { // РАЗОБРАТЬСЯ В ПЕРВОМ IF`е
+    secretCode = [];
+  }
+  secretCode.push(event.code);
+  if(password.join() === secretCode.join()) {
+    secret.classList.add('visible');
+  }
+}
+
+document.addEventListener('keydown', showSecretCode);
